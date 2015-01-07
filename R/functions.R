825a471684099fcf2d2156266f88a4abbe6948a4
Clark Rushing <crushing05@gmail.com>
2015-01-07 10:36:51 -0500

    Added rjags

---
+++ R/functions.R
@@ -0,0 +1,54 @@
+#' Standard Error
+#'
+#' Computes standard error of a vector
+#' @param x Numeric vector
+#' @keywords standard error
+#' @export
+#' @examples
+#' x <- rnorm(100)
+#' se(x)
+
+se <- function(x){
+  sd(x)/sqrt(length(x))
+}
+
+#' Mean
+#'
+#' Same as base mean, but defaults to printing 3 digits
+#' @param x Numeric vector
+#' @keywords mean
+#' @export
+#' @examples
+#' x <- rnorm(100)
+#' mean(x)
+
+mean <- function(x) {
+  mu <- base::mean(x, na.rm=TRUE)
+  mu3 <- print(mu, digits=3)
+}
+
+#' %out%
+#'
+#' Reverse of %in% - determine whether values are not in a vector
+#' @param x Value or vector of interest
+#' @param y Vector of values to compare to x
+#' @keywords out
+#' @export
+#' @examples
+#' x <- 11
+#' y <- seq(1:10)
+#' x%out%y
+#' [1] TRUE
+
+"%out%" <- function(x,y){
+  !(x %in% y)
+}
+
+#' Inverse logit
+#'
+#' Function to compute inverse logit
+#' @param x vector of logit transformed numerical values
+#' @keywords logit; logistic regression
+#' @export
+
+invlogit <- function(x){exp(x)/(1+exp(x))}
---
+++ R/zzz.R
@@ -0,0 +1,60 @@
+.onLoad <- function(libname="crushingr", pkgname="crushingr"){
+  library(ggplot2)
+  theme_clark <- function(base_size = 12, base_family = "") {
+    theme(
+      # Elements in this first block aren't used directly, but are inherited
+      # by others
+      line =               element_line(colour = "black", size = 0.5, linetype = 1,
+                                        lineend = "butt"),
+      rect =               element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
+      text =               element_text(family = base_family, face = "plain",
+                                        colour = "black", size = base_size,
+                                        hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
+      axis.text =          element_text(size = rel(0.8), colour = "grey50"),
+      strip.text =         element_text(size = rel(0.8)),
+
+      axis.line =          element_line(size=.7, color = "black"),
+      axis.text.x =        element_text(size = base_size , lineheight = 0.9, vjust = 1),
+      axis.text.y =        element_text(size = base_size, lineheight = 0.9, hjust = 1.2),
+      axis.ticks =         element_line(colour = "black", size = 0.2),
+      axis.title.x =       element_text(size = base_size*1.4, vjust = 0.3),
+      axis.title.y =       element_text(size = base_size*1.4, angle = 90, vjust = 1),
+      axis.ticks.length =  grid::unit(0.3, "lines"),
+      axis.ticks.margin =  grid::unit(0.5, "lines"),
+
+      legend.background =  element_rect(colour = NA),
+      legend.margin =      grid::unit(0.2, "cm"),
+      legend.key =         element_rect(colour = "grey80"),
+      legend.key.size =    grid::unit(1.2, "lines"),
+      legend.key.height =  NULL,
+      legend.key.width =   NULL,
+      legend.text =        element_text(size = base_size * 0.8),
+      legend.text.align =  NULL,
+      legend.title =        element_text(size = base_size * 0.8, face = "bold", hjust = 0),
+      legend.title.align = NULL,
+      legend.position =    "right",
+      legend.direction =   NULL,
+      legend.justification = "center",
+      legend.box =         NULL,
+
+      panel.background =   element_rect(fill = "white", colour = NA),
+      panel.border =       element_rect(fill=NA, color="grey80",size=.5),
+      panel.grid.major =   element_line(colour = "grey90", size = 0.2),
+      panel.grid.minor =   element_blank(),
+      panel.margin =       grid::unit(0.25, "lines"),
+      panel.margin.x =     NULL,
+      panel.margin.y =     NULL,
+
+      strip.background =   element_rect(fill = NA, colour = NA),
+      strip.text.x =       element_text(size = base_size),
+      strip.text.y =       element_text(size = base_size, angle = -90),
+
+      plot.background =   element_rect(colour = NA),
+      plot.title =        element_text(size = base_size * 1.7, face="bold",vjust=1),
+      plot.margin =       grid::unit(c(1, 1.5, 0.8, 0.8), "lines"),
+
+      complete = TRUE
+    )
+  }
+  theme_set(theme_clark())
+}
