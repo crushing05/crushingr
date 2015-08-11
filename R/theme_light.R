376038a83ab3e49f6749e6570e8da8ad521ddfee
Clark Rushing <crushing05@gmail.com>
2015-08-10 21:47:06 -0400

    Adjust axis text color

--- R/theme_light.R
+++ R/theme_light.R
@@ -13,10 +13,10 @@ theme_light <- function(base_size = 12, base_family = "") {
                                       lineend = "butt"),
     rect =               element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
     text =               element_text(family = base_family, face = "plain",
-                                      colour = "#839496", size = base_size,
+                                      colour = "#586e75", size = base_size,
                                       hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
     axis.text =          element_text(size = rel(0.8), colour = "#586e75"),
-    axis.title=          element_text(colour = "#839496", vjust=0.35),
+    axis.title=          element_text(colour = "#586e75", vjust=0.35),
     strip.text =         element_text(size = rel(0.8)),
 
     axis.line =          element_line(size=.7, color = "#586e75"),
@@ -62,3 +62,8 @@ theme_light <- function(base_size = 12, base_family = "") {
     complete = TRUE
   )
 }
+
+require(ggplot2)
+df <- data.frame(x = seq(1:10), y = seq(1:10))
+
+ggplot(df, aes(x =x, y = y)) + geom_point()+theme_light()
