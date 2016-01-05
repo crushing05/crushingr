24cc47dea0262ccaec0e2da019210c3ca6229980
Clark Rushing <crushing05@gmail.com>
2016-01-05 15:56:40 -0500

    Update default theme to work w/ ggplot 2.0

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,7 @@
   options(digits=4)
   library(ggplot2)
   theme_clark <- function(base_size = 12, base_family = "") {
+    half_line <- base_size/2
     theme(
       # Elements in this first block aren't used directly, but are inherited
       # by others
@@ -10,19 +11,24 @@
       rect =               element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
       text =               element_text(family = base_family, face = "plain",
                                         colour = "black", size = base_size,
-                                        hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
+                                        hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9, margin = margin(),
+                                        debug = FALSE),
       axis.text =          element_text(size = rel(0.8), colour = "grey30"),
       axis.title=          element_text(colour = "grey10", vjust=0.35),
       strip.text =         element_text(size = rel(0.8)),
 
       axis.line =          element_line(size=.7, color = "black"),
-      axis.text.x =        element_text(size = base_size*1.4 , lineheight = 0.9),
-      axis.text.y =        element_text(size = base_size*1.4, lineheight = 0.9),
+      axis.text.x =        element_text(size = base_size*1.4 , lineheight = 0.9,
+                                        margin = margin(t = 0.8 * half_line/2), vjust = 1),
+      axis.text.y =        element_text(size = base_size*1.4, lineheight = 0.9,
+                                        margin = margin(r = 0.8 * half_line/2), vjust = 1),
       axis.ticks =         element_line(colour = "black", size = 0.2),
-      axis.title.x =       element_text(size = base_size*1.7, vjust = 0.3),
-      axis.title.y =       element_text(size = base_size*1.7, angle = 90, vjust = 1),
+      axis.title.x =       element_text(size = base_size*1.7, vjust = 0.3,
+                                        margin = margin(t = 0.8 * half_line, b = 0.8 * half_line/2)),
+      axis.title.y =       element_text(size = base_size*1.7, angle = 90, vjust = 1,
+                                        margin = margin(r = 0.8 * half_line, l = 0.8 * half_line/2)),
       axis.ticks.length =  grid::unit(0.3, "lines"),
-      axis.ticks.margin =  grid::unit(0.5, "lines"),
+      #axis.ticks.margin =  grid::unit(0.5, "lines"),
 
       legend.background =  element_rect(colour = NA),
       legend.margin =      grid::unit(0.2, "cm"),
@@ -40,19 +46,22 @@
       legend.box =         NULL,
 
       panel.background =   element_rect(fill = "white", colour = NA),
-      panel.border =       element_rect(fill=NA, color="grey80",size=.5),
+      panel.border =       element_rect(fill = NA, color = NA,size=.5),
       panel.grid.major =   element_blank(),
       panel.grid.minor =   element_blank(),
-      panel.margin =       grid::unit(0.25, "lines"),
+      panel.margin =       NULL,
       panel.margin.x =     NULL,
       panel.margin.y =     NULL,
+      panel.ontop =        FALSE,
 
       strip.background =   element_rect(fill = NA, colour = NA),
-      strip.text.x =       element_text(size = base_size),
-      strip.text.y =       element_text(size = base_size, angle = -90),
+      strip.text.x =       element_text(size = base_size, margin = margin(t = half_line, b = half_line)),
+      strip.text.y =       element_text(size = base_size, angle = -90, margin = margin(l = half_line, r = half_line)),
+      strip.switch.pad.grid = unit(0.1, "cm"),
+      strip.switch.pad.wrap = unit(0.1, "cm"),
 
       plot.background =   element_rect(colour = NA),
-      plot.title =        element_text(size = base_size * 1.7, face="bold",vjust=2),
+      plot.title =        element_text(size = base_size * 1.7, face="bold",vjust=2, margin = margin(b = half_line * 1.2)),
       plot.margin =       grid::unit(c(1, 1.5, 0.8, 0.8), "lines"),
 
       complete = TRUE
